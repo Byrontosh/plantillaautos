57c85464da1dcc220ceb48318ec715d9e23b635d
BYRON <by_tosh20@hotmail.com>
2020-09-06 07:11:59 -0500

    plantilla

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+const routes:Routes = [
+  { path:'', component:PagesComponent,
+    children:[
+  {path: 'dashboard', component:DashboardComponent},
+  {path: '', redirectTo:'/dashboard',pathMatch:'full'},
+    ]
+  },
+  
+  {path: 'login', component:LoginComponent},
+  {path: 'register', component:RegisterComponent},
+
+  
+  {path: '**', component:NopagefoundComponent},
+
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    
+    RouterModule.forRoot(routes)
+    
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    PagesComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
